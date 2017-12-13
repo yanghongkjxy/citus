5c89b3fd29f9e6a4fee4dddf089e53ab7a487ec3
Onder Kalaci <onder@citusdata.com>
2017-12-13 16:20:53 +0200

    Recursively plan subqueries that are not safe to pushdown
    With this commit, Citus recursively plans subqueries that
    are not safe to pushdown, in other words, requires a merge
    step.
    
    The algorithm is simple: Recursively traverse the query from bottom
    up (i.e., bottom meaning the leaf queries). On each level, check
    whether the query is safe to pushdown (or a single repartition
    subquery). If the answer is yes, do not touch that subquery. If the
    answer is no, plan the subquery seperately (i.e., create a subPlan
    for it) and replace the subquery with a call to
    `read_intermediate_results(planId, subPlanId)`. During the the
    execution, run the subPlans first, and make them avaliable to the
    next query executions.
    
    Some of the queries hat this change allows us:
    
       * Subqueries with LIMIT
       * Subqueries with GROUP BY/DISTINCT on non-partition keys
       * Subqueries involving re-partition joins, router queries
       * Mixed usage of subqueries and CTEs (i.e., use CTEs in
         subqueries as well). Nested subqueries as long as we
         support the subquery inside the nested subquery.
       * Subqueries with local tables (i.e., those subqueries
         has the limitation that they have to be leaf subqueries)
    
       * VIEWs on the distributed tables just works (i.e., the
         limitations mentioned below still applies to views)
    
    Some of the queries that is still NOT supported:
    
      * Corrolated subqueries that are not safe to pushdown
      * Window function on non-partition keys
      * Recursively planned subqueries or CTEs on the outer
        side of an outer join
      * Only recursively planned subqueries and CTEs in the FROM
        (i.e., not any distributed tables in the FROM) and subqueries
        in WHERE clause
      * Subquery joins that are not on the partition columns (i.e., each
        subquery is individually joined on partition keys but not the upper
        level subquery.)
      * Any limitation that logical planner applies such as aggregate
        distincts (except for count) when GROUP BY is on non-partition key,
        or array_agg with ORDER BY

--- src/backend/distributed/planner/multi_explain.c
+++ src/backend/distributed/planner/multi_explain.c
@@ -34,6 +34,7 @@
 #include "distributed/multi_server_executor.h"
 #include "distributed/remote_commands.h"
 #include "distributed/placement_connection.h"
+#include "distributed/recursive_planning.h"
 #include "distributed/worker_protocol.h"
 #include "lib/stringinfo.h"
 #include "nodes/plannodes.h"
